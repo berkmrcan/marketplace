a3cda2a9d8ab3fa37f4a32e5c9e2159f79304f37
berkmrcan <140258756+berkmrcan@users.noreply.github.com>
2025-01-27 12:18:46 +0300

    register models to admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser
+from models import CustomUser, ShippingAddress
 # Register your models here.
 
 admin.register(CustomUser)
+admin.register(ShippingAddress)
